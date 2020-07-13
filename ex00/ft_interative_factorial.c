cadc50c1381e1803a92b4b4f94f05702973238b7
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r6s3.wethinkcode.co.za>
2020-07-13 09:15:04 +0200

    exercise 00 completed

---
+++ ex00/ft_interative_factorial.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_interative_factorial.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 08:38:25 by anmugwab          #+#    #+#             */
+/*   Updated: 2020/07/13 09:14:14 by anmugwab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_interative_factorial(int nb)
+{
+	int count;
+	int result;
+
+	count = 1;
+	result = nb;
+
+	if (nb < 0)
+		return (0);
+
+	if (nb == 0)
+		return (count);
+
+	while (count < nb)
+	{
+		result = result * (nb - count);
+		count++;
+	}
+
+	return (result);
+}
