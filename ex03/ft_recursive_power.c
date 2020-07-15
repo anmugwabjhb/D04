7f6cb26aa145f37919507a512d6cf4fe45f894e5
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r6s3.wethinkcode.co.za>
2020-07-15 10:10:37 +0200

    correction on ex03

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,18 +6,17 @@
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 14:24:21 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/13 15:59:19 by anmugwab         ###   ########.fr       */
+/*   Updated: 2020/07/15 10:10:03 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	 ft_recursive_power(int nb, int power)
 {
-		if (power < 0)
-			return	(0);
+	if (power < 0)
+		return	(0);
 
-		if (power > 0)
-			return (nb * ft_recursive_power(nb, power - 1));
-		else 
-			return (1);	
-
+	if (power > 0)
+		return (nb * ft_recursive_power(nb, power - 1));
+	else 
+		return (1);	
 }
