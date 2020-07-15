fd229366820b9a7dbd8f528bde49a41110683e3b
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r6s3.wethinkcode.co.za>
2020-07-15 10:08:45 +0200

    correction on ex02

--- ex02/ft_interative_power.c
+++ ex02/ft_interative_power.c
@@ -6,15 +6,15 @@
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 11:40:14 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/13 11:59:53 by anmugwab         ###   ########.fr       */
+/*   Updated: 2020/07/15 10:08:01 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-int ft_interative_power(int nb, int power)
+int 	ft_interative_power(int nb, int power)
 {
-	int 		count;
-	int			answer;
+	int	count;
+	int	answer;
 
 	count	=	power;
 	answer	=	1;
